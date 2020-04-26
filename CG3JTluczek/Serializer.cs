ab7d12aafece5505b0479f4b3aea4c360ea00468
tluczekk <35299142+tluczekk@users.noreply.github.com>
2020-04-26 15:12:14 +0200

    Completed todo

---
+++ CG3JTluczek/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG3JTluczek
+{
+    public class Serializer
+    {
+        public RasterGraphicsWrapper Load(string filename)
+        {
+            RasterGraphicsWrapper temp = new RasterGraphicsWrapper();
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            temp = (RasterGraphicsWrapper)formatter.Deserialize(stream);
+            stream.Close();
+            return temp;
+        }
+        public void Save(string filename, RasterGraphicsWrapper ras)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, ras);
+            stream.Close();
+        }
+
+    }
+}
